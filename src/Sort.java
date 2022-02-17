6da4111eac1855682ca239ca2fed61ee52958ff7
BT1E10_18 <sample@bths.edu>
2022-02-17 14:00:37 -0500

    started lab

--- src/Sort.java
+++ src/Sort.java
@@ -3,27 +3,32 @@ import java.util.ArrayList;
 public class Sort
 {
   // Part A.  Which Sort is Which?
-  public static void firstSortingAlgorithm(int[] elements)
+  public static void selectionSort(int[] elements)
   {
+    int count = 0;
     for (int j = 0; j < elements.length - 1; j++)
     {
       int minIndex = j;
       for (int k = j + 1; k < elements.length; k++)
-      {      
+      {
+        count++;
         if (elements[k] < elements[minIndex])
         {
           minIndex = k;
+
         }
       }
       int temp = elements[j];
       elements[j] = elements[minIndex];
       elements[minIndex] = temp;
     }
+    System.out.println("Number of iterations: " + count);
   }
 
   // Part A.  Which Sort is Which?
-  public static void secondSortingAlgorithm(int[] elements)
-  { 
+  public static void insertionSort(int[] elements)
+  {
+    int count = 0;
     for (int j = 1; j < elements.length; j++)
     {
       int temp = elements[j];
@@ -32,9 +37,12 @@ public class Sort
       {     
         elements[possibleIndex] = elements[possibleIndex - 1];
         possibleIndex--;
+        count++;
       }
       elements[possibleIndex] = temp;
     }
+    System.out.println("Number of iterations: " + count);
+
   }
   
   // Part C.  Sorting a 1000-word list!
